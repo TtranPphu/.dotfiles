508f0c71143b371396e63879d223ffc3711fb313
TtranPphu <ttranpphu0212@gmail.com>
2026-05-06 20:49:30 +0700

    Hyprland - Prevent disable the last screen

--- hypr/.config/hypr/scripts/toggle-internal-display.sh
+++ hypr/.config/hypr/scripts/toggle-internal-display.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-if [ -f ~/.local/state/hypr/toggles/internal-display-disable ]; then
-  rm ~/.local/state/hypr/toggles/internal-display-disable
+if [[ -f ~/.local/state/hypr/toggles/internal-display-disable || $(hyprctl monitors | grep -c "Monitor") -le 1 ]]; then
+  rm -f ~/.local/state/hypr/toggles/internal-display-disable
   hyprctl keyword monitor "eDP-1,preferred,auto,2"
   hyprctl keyword monitor "eDP-2,preferred,auto,2"
 else
