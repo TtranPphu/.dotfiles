6fd1cf2a4912853bd98615a34bface3bf835cc59
TtranPphu <ttranpphu0212@gmail.com>
2026-05-11 22:37:27 +0700

    Hypr: Stack home monitor

--- hypr/.config/hypr/scripts/init-displays.sh
+++ hypr/.config/hypr/scripts/init-displays.sh
@@ -3,6 +3,5 @@
 if [[ $(hyprctl monitors all | grep -c "Monitor") -ge 2 ]]; then
   lock_file=~/.local/state/hypr/toggles/internal-display-disable
   [ -e $lock_file ] || touch $lock_file
-  hyprctl keyword monitor "eDP-1,disable"
-  hyprctl keyword monitor "eDP-2,disable"
+  hyprctl keyword monitor "eDP-2,preferred,auto-center-down,2"
 fi
--- hypr/.config/hypr/scripts/toggle-internal-display.sh
+++ hypr/.config/hypr/scripts/toggle-internal-display.sh
@@ -3,5 +3,5 @@
 if [[ $(hyprctl monitors | grep Monitor | grep -c eDP-2) -ge 1 && $(hyprctl monitors all | grep -c Monitor) -ge 2 ]]; then
   hyprctl keyword monitor "eDP-2,disable"
 else
-  hyprctl keyword monitor "eDP-2,preferred,auto,2"
+  hyprctl keyword monitor "eDP-2,preferred,auto-center-down,2"
 fi
